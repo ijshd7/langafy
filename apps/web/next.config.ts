a91a4100514bb3a5209c71ff43a909b1634eb8fa
isherrill <me@isaiahsherrill.com>
2026-03-13 14:23:49 -0500

    chore(web,docker): add dockerfile for web

--- apps/web/next.config.ts
+++ apps/web/next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
+import path from "path";
 
 const nextConfig: NextConfig = {
+  output: "standalone",
   transpilePackages: ["@langafy/shared-types"],
+  turbopack: {
+    root: path.resolve(__dirname, "../../"),
+  },
 };
 
 export default nextConfig;
